756c63c4fc9495f65546b1de88e4a4aaffd39143
sajjal <nairsajjal@gmail.com>
2019-06-26 15:36:09 +0530

    frontend completed

--- src/App.js
+++ src/App.js
@@ -5,22 +5,38 @@ import './App.css';
 import CreateTodo from './components/create-todo.component';
 import EditTodo from './components/edit-todo.component'
 import TodosList from './components/todos-list.component'
-
+import Logo from './logo.png';
 function App() {
   return (
-   <div className="container">
-     <Router>
+    <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={Logo} width="40" height="40" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+</Router>
+   
+       
       
-      
-      
-        <h2>Todo_App</h2>
-      
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" exact component={EditTodo} />
-      <Route path="/create" exact component={CreateTodo} />
-      
-      </Router>
-   </div>
+     
   );
 }
 
